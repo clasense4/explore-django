f3cfa612ded6245cdbd7b1ea685d0d3e12a3cca5
Fajri Abdillah <clasense4@gmail.com>
2020-08-02 01:44:51 +0700

    Improve coverage to 73%

--- src/photos/tests/test_views.py
+++ src/photos/tests/test_views.py
@@ -18,30 +18,30 @@ class RegisterUserTests(TestCase):
             {'username':'fajri1','password1':'passwordfajri1','password2':'passwordfajri1'},
             format='json'
         )
-        print(response.data)
         self.user_token = response.data['token']
 
     def test_00_get_auth_token_user(self):
+        # Get auth token
         response = client.post(
             '/api-token-auth/',
             {'username':'fajri1','password':'passwordfajri1'},
             format='json'
         )
         self.assertIs(True, status.is_success(response.status_code))
-        # self.assertEqual(self.user_token, response.data['token'])
 
     def test_01_get_user_photo_no_result(self):
+        # New created user doesn't have any photo
         client = APIClient()
         client.credentials(HTTP_AUTHORIZATION='JWT ' + self.user_token)
         response = client.get(
             '/api/v1/photo/'
         )
-
         photo_count = len(response.data)
         self.assertIs(status.is_success(response.status_code), True)
         self.assertIs(photo_count, 0)
 
     def test_02_post_published_photo_user(self):
+        # Post a new published photo
         client = APIClient()
         client.credentials(HTTP_AUTHORIZATION='JWT ' + self.user_token)
         with open('photos/tests/sapi.jpg', 'rb') as fp:
@@ -55,18 +55,25 @@ class RegisterUserTests(TestCase):
                 },
                 format='multipart'
             )
-
-        print(response.data)
+        obj_id = response.data['id']
         self.assertIs(status.is_success(response.status_code), True)
+
+        # Get all photo
         response = client.get(
             '/api/v1/photo/'
         )
-
         photo_count = len(response.data)
         self.assertIs(status.is_success(response.status_code), True)
         self.assertIs(photo_count, 1)
 
+        # Get individual photo
+        response = client.get(
+            '/api/v1/photo/' + str(obj_id) + '/'
+        )
+        self.assertIs(status.is_success(response.status_code), True)
+
     def test_03_post_draft_photo_user(self):
+        # Post a new draft photo
         client = APIClient()
         client.credentials(HTTP_AUTHORIZATION='JWT ' + self.user_token)
         with open('photos/tests/sapi.jpg', 'rb') as fp:
@@ -80,13 +87,24 @@ class RegisterUserTests(TestCase):
                 },
                 format='multipart'
             )
-
-        print(response.data)
+        obj_id = response.data['id']
         self.assertIs(status.is_success(response.status_code), True)
-        response = client.get(
-            '/api/v1/photo/'
-        )
 
-        photo_count = len(response.data)
-        self.assertIs(status.is_success(response.status_code), True)
-        self.assertIs(photo_count, 1)
+        # TODO: Get all photo with status draft with query string
+        # TODO: Get individual photo
+
+    def test_04_bad_request_post(self):
+        # Post a bad request photo
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION='JWT ' + self.user_token)
+        with open('photos/tests/sapi.jpg', 'rb') as fp:
+            response = client.post(
+                '/api/v1/photo/',
+                {
+                    'file': fp,
+                    'captions': 'Sapi australia #sapi',
+                },
+                format='multipart'
+            )
+        self.assertIs(status.is_client_error(response.status_code), True)
+
