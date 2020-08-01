5ce351b0a7d2a2718e1a70c111f1a1182e1732c0
Fajri Abdillah <clasense4@gmail.com>
2020-08-02 01:08:15 +0700

    Add model and add it to admin

--- src/photos/models.py
+++ src/photos/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+PHOTO_STATUS = [
+    ('d', 'Draft'),
+    ('p', 'Published'),
+]
+
+class Photo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    captions = models.TextField()
+    file = models.ImageField(blank=False, null=False)
+    status = models.CharField(max_length=1, choices=PHOTO_STATUS)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
