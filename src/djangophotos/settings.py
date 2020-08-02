0549d1310ae6359dc4ee478a3567f923745aa34a
Fajri Abdillah <clasense4@gmail.com>
2020-08-03 01:14:42 +0700

    Update docker compose prod

--- src/djangophotos/settings.py
+++ src/djangophotos/settings.py
@@ -135,8 +135,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+  os.path.join(SITE_ROOT, 'static/'),
+)
 
 REST_FRAMEWORK = {
     # Authentication Scheme
